854507b31e10a9d4cf2af3584e8db1f440e9c52d
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2026-03-07 22:57:26 -0800

    stashing some changes; starting to outline the ui loop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,58 @@ mod state;
 mod ui;
 mod app;
 
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{
+        disable_raw_mode, 
+        enable_raw_mode, 
+        EnterAlternateScreen, 
+        LeaveAlternateScreen,
+    },
+};
+use std::io;
+use ratatui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
+    // enable raw mode to capture keystrokes
+    enable_raw_mode()?;
+
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let result = run_app(&mut terminal);
+    
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    terminal.show_cursor()?;
+
+    result
+}
+
+fn run_app<B: ratatui::backend::Backend>(
+    terminal: &mut Terminal<B>,
+) -> Result<(), io::Error> {
+    loop {
+        terminal.draw(|frame| {
+            let size = frame.size();
+
+            let block = ratatui::widgets::Block::default()
+                .title("Live Baseball Scoreboard TUI")
+                .borders(ratatui::widgets::Borders::ALL);
+
+            frame.render_widget(block, size);
+        })?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                break;
+            }
+        }
+    }
+    Ok(())
 }
